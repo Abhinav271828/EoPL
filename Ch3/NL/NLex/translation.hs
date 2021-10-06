d7fc2695d773a414e8189024c429e931584aea55
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2021-10-06 16:02:49 +0530

    Nameless exercises except actual last

--- Ch3/NL/NLex/translation.hs
+++ Ch3/NL/NLex/translation.hs
@@ -48,7 +48,7 @@ translationof (Cdr e) s = NLCdr (translationof e s)
 translationof (Iszero e) s = NLIszero (translationof e s)
 translationof (Ifte c t e) s = NLIfte (translationof c s) (translationof t s) (translationof e s)
 translationof (Cond es) s = NLCond (map (\(i,e) -> (translationof i s, translationof e s)) es)
-translationof (Lets es b) s = NLLets (map (\(_,e) -> translationof e s) es) (translationof b (extendsenv (map fst es) s))
+-- translationof (Lets es b) s = NLLets (map (\(_,e) -> translationof e s) es) (translationof b (extendsenv (map fst es) s))
 translationof (Letrec fs b) s = NLLetrec
                                       (map (\(v,as,e) ->
                                                 translationof e (extendsenv as names))
@@ -57,3 +57,32 @@ translationof (Letrec fs b) s = NLLetrec
                                                 where names = extendsenv (map (\(v,_,_) -> v) fs) s
 translationof (ProcE vs b) s = NLProc (translationof b (extendsenv vs s))
 translationof (CallE f as) s = NLCall (translationof f s) (map (\a -> translationof a s) as)
+translationof (Lets es b) s = let functions = filter (\(_,e) -> isProc e) es
+                                  variables = filter (\(_,e) -> not $ isProc e) es
+                                  body = replace functions b
+                              in NLLets (map (\(_,e) -> translationof e s) variables) (translationof body
+                                                                                         (extendsenv (map fst variables) s))
+
+isProc :: AST -> Bool
+isProc p = case p of
+             ProcE _ _ -> True
+             _ -> False
+
+replace :: [(String,AST)] -> AST -> AST
+replace [] p = p
+replace l@((n,val):fs) p = let prog = replace fs p
+                               ProcE args body = val
+                           in case prog of
+                                Var n -> val
+                                Diff e1 e2 -> Diff (replace l e1) (replace l e2)
+                                Cons e1 e2 -> Cons (replace l e1) (replace l e2)
+                                Car e -> Car (replace l e)
+                                Cdr e -> Cdr (replace l e)
+                                Iszero e -> Iszero (replace l e)
+                                Lets xs b -> Lets (map (\(x,y) -> (x, replace l y)) xs) (replace l b)
+                                Letrec xs b -> Letrec (map (\(x,y,z) -> (x, y, replace l z)) xs) (replace l b)
+                                Ifte c t e -> Ifte (replace l c) (replace l t) (replace l e)
+                                Cond cs -> Cond (map (\(x,y) -> (replace l x, replace l y)) cs)
+                                ProcE as b -> ProcE as (replace l b)
+                                CallE f as -> CallE (replace l f) (map (replace l) as)
+                                _ -> prog
