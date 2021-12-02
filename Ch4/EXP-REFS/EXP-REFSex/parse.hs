9901c7de9f4058d918f3f3f8200a63877c4eb9df
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2021-12-02 14:48:36 +0530

    Multi-arg functions

---
+++ Ch4/EXP-REFS/EXP-REFSex/parse.hs
@@ -0,0 +1,98 @@
+module Parse where
+
+import Data.Char
+
+-- Expression Datatype --
+data AST = Seq [AST]                            |
+           Newref AST | Deref AST               |
+           Setref AST AST                       |
+           RefE Int                             |
+           Const Int                            |
+           Cons AST AST | EmpL                  |
+           Car AST | Cdr AST                    |
+           Var String                           |
+           Diff AST AST                         |
+           Iszero AST                           |
+           Let String AST AST                   |
+           Letrec [(String, [String], AST)] AST   |
+           Ifte AST AST AST                     |
+           ProcE [String] AST                   |
+           CallE AST [AST]
+           deriving Show
+
+scanparse :: String -> AST
+scanparse prog = let (p, []) = parse (words prog)
+                 in p
+
+parse :: [String] -> (AST, [String])
+parse lex = case lex of
+              ("begin":ls) -> (Seq qs, rest)
+                                 where (qs, rest) = parsexps ls
+                                       parsexps xs = (p:ps, rs)
+                                                        where (p, rem) = parse xs
+                                                              (ps,rs) = case rem of
+                                                                           ";":rst -> parsexps rst
+                                                                           "end":rst -> ([], rst)
+              ("newref":"(":ls) -> (Newref v, rest)
+                                 where (v, ")":rest) = parse ls
+              ("deref":"(":ls) -> (Deref v, rest)
+                                 where (v, ")":rest) = parse ls
+              ("setref":"(":ls) -> (Setref ref exp, rest)
+                                        where (ref, ",":rem) = parse ls
+                                              (exp, ")":rest) = parse rem
+              ("let":v:"=":ls) -> (Let v (assm) (body), rest)
+                                     where (assm, "in":rem) = parse ls
+                                           (body, rest) = parse rem
+              ("-":"(":ls) -> (Diff op1 op2, rest)
+                                 where (op1, ",":rem) = parse ls
+                                       (op2, ")":rest) = parse rem
+              ("[]":ls) -> (EmpL, ls)
+              ("cons":ls) -> (Cons car cdr, rest)
+                                where (car, rem) = parse ls
+                                      (cdr, rest) = parse rem
+              ("car":ls) -> (Car body, rest)
+                                  where (body, rest) = parse ls
+              ("cdr":ls) -> (Cdr body, rest)
+                                  where (body, rest) = parse ls
+              ("iszero":ls) -> (Iszero body, rest)
+                                  where (body, rest) = parse ls
+              ("if":ls) -> (Ifte c t e, rest)
+                              where (c, "then":rem) = parse ls
+                                    (t, "else":erem) = parse rem
+                                    (e, rest) = parse erem
+              ("λ":ls) -> (ProcE args body, rest)
+                                     where (args, "->":rem) = parseargs ls
+                                           (body, rest) = parse rem
+                                           parseargs as = (a:r, s)
+                                                            where (a:x) = as
+                                                                  (r,s) = case x of
+                                                                        ",":rs -> parseargs rs
+                                                                        "->":_ -> ([], x)
+              ("letrec":ls) -> (Letrec funs body, rest)
+                               where (funs, "in":rem) = parsefuns ls
+                                     (body, rest) = parse rem
+                                     parsefuns fs = ((x,as,b):r, rs)
+                                                    where (x:"=":"λ":args) = fs
+                                                          (as, "->":s) = parseargs args
+                                                          (b, t) = parse s
+                                                          (r, rs) = case t of
+                                                                     ",":ts -> parsefuns ts
+                                                                     "in":_ -> ([],t)
+                                                          parseargs bs = (b:u, v)
+                                                                         where (b:y) = bs
+                                                                               (u,v) = case y of
+                                                                                        ",":ys -> parseargs ys
+                                                                                        "->":_ -> ([],y)
+              (l:ls) -> if (all isDigit l)
+                          then (Const (read l :: Int), ls)
+                          else if (head l == 'r')
+                                 then (RefE (read (tail l) :: Int), ls)
+                                 else case ls of
+                                   "(":r -> (CallE (Var l) (args), rest)
+                                            where (args, ")":rest) = parseargs r
+                                                  parseargs as = (a:r, s)
+                                                                    where (a,x) = parse as
+                                                                          (r,s) = case x of
+                                                                                   ",":rs -> parseargs rs
+                                                                                   ")":_ -> ([],x)
+                                   _ -> (Var l, ls)
