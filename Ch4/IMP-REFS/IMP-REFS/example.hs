9a824d6f85cf311108aa7a13bd24db337d1f8d56
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2021-12-12 18:47:30 +0530

    Fix examples

--- Ch4/IMP-REFS/IMP-REFS/example.hs
+++ Ch4/IMP-REFS/IMP-REFS/example.hs
@@ -1,12 +1,23 @@
 import Parse
 import Interpret
 
-p :: String -- evaluates to 1
-p = "let g = let c = 10 \
-            \in λ * -> begin \
-                        \set c = - ( c , 1 ) ; \
-                        \c \
-                      \end \
-    \in let a = g ( 11 ) \
-       \in let b = g ( 11 ) \
-          \in - ( a , b )"
+p :: String -- evaluates to 0
+p = "letrec 0mod2 = λ x -> if iszero x \
+                          \then 1 \
+                          \else 1mod2 ( - ( x , 1 ) ) , \
+           \1mod2 = λ x -> if iszero x \
+                          \then 0 \
+                          \else 0mod2 ( - ( x , 1 ) ) \
+    \in 0mod2 ( 5 )"
+
+q :: String -- evaluates to 1
+q = "letrec 0mod3 = λ x -> if iszero x \
+                          \then 1 \
+                          \else 2mod2 ( - ( x , 1 ) ) , \
+           \1mod3 = λ x -> if iszero x \
+                          \then 0 \
+                          \else 0mod2 ( - ( x , 1 ) ) \
+           \2mod3 = λ x -> if iszero x \
+                          \then 0 \
+                          \else 1mod2 ( - ( x , 1 ) ) \
+    \in 0mod3 ( 9 )"
